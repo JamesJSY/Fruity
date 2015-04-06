3ab247fd0ec9776e78d885283a039ca0713ff1d4
JamesJSY <shiyuanjiang@Shiyuans-MacBook-Air.local>
2015-04-05 23:22:07 -0400

    Initial Commit

---
+++ Fruity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fruity
+//
+//  Created by Shiyuan Jiang on 4/5/15.
+//  Copyright (c) 2015 Shiyuan Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fruity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fruity
+//
+//  Created by Shiyuan Jiang on 4/5/15.
+//  Copyright (c) 2015 Shiyuan Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
